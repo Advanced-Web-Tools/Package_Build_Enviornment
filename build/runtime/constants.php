c8d5d89152d3f75cb76579c911db9e5b9fd06005
ElStefanos <stefancrkvenjakov1@gmail.com>
2025-12-06 19:29:13 +0100

    Quick fix

--- build/runtime/constants.php
+++ build/runtime/constants.php
@@ -1,12 +1,12 @@
 <?php
 
-const NAME = '';
-const MANIFEST = NAME . '/manifest.json';
+define('NAME', '');
+define('MANIFEST', NAME . '/manifest.json');
 
-const COLOR_RESET = "\033[0m";
-const COLOR_RED = "\033[31m";
-const COLOR_GREEN = "\033[32m";
-const COLOR_YELLOW = "\033[33m";
+define('COLOR_RESET', "\033[0m");
+define('COLOR_RED', "\033[31m");
+define('COLOR_GREEN', "\033[32m");
+define('COLOR_YELLOW', "\033[33m");
 
 function color(string $text, string $color): string {
     return $color . $text . COLOR_RESET;
